46aa259646aad0b802b9213d0fe45cb01fb916d2
rakibul114 <rakibulislam114@gmail.com>
2022-04-27 01:33:13 +0600

    shipping form created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,15 @@ import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
   const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  };
+  
     return (
       <nav className="header">
         <img src={logo} alt="" />
@@ -17,7 +23,7 @@ const Header = () => {
           <Link to="/about">About</Link>
           
           {
-            user ? <button>Sign Out</button> : 
+            user ? <button onClick={handleSignOut}>Sign Out</button> : 
             <Link to='/login'>Login</Link>
           }
         </div>
